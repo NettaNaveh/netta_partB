f9a321f752dfaa97638eb95833be7f73df8e3abb
netta <nettanaveh10@gmail.com>
2022-12-16 21:46:17 +0200

    8 push

--- functions.js
+++ functions.js
@@ -1,16 +1,5 @@
 
 
-class Person {
-    constructor(firstName, lastName, Email, Phone) {
-        this.firstName = firstName
-        this.lastName = lastName
-        this.Email = Email
-        this.Phone = Phone
-    }
-}
-    const person1 = new Person('Inbal', 'Barbi', 'inbal@gmail.com', '0524545454')
-    const person2 = new Person('Itai', 'Naveh', 'itai@gmail.com', '0524548888')
-
 let ShoppingList = [];
 
 // vegetables
@@ -23,24 +12,30 @@ const items = [
         id: 1
     },
     {
-        image: "../media/chalah/chalah.jpeg",
+        image: "tomato.jpg",
         name: "Tomato",
         price: 4,
         category: 'Garden vegetables',
         id: 2
     },
     {
-        image: "../media/chalah/chalah.jpeg",
+        image: "salanova.jpg",
         name: "Salanova lettuce",
         price: 6.3,
         category: 'Lettuce',
         id: 3
     },    {
-        image: "../media/chalah/chalah.jpeg",
+        image: "portabelo.jpg",
         name: "Portobello",
         price: 5,
         category: 'Mushrooms',
         id: 4
+    }, {
+        image: "mint.jpg",
+        name: "Mint",
+        price: 10,
+        category: 'Herbs',
+        id: 5
     }]
 
 //show all items on home page-index
@@ -66,9 +61,6 @@ function showItems() {
         } else if (item.category === 'Mushrooms') {
             createCard(MushroomsCategory, items[i])
         }
-        // else {
-        //     createCard(Stars, items[i]);
-        // }
     }
 }
 
@@ -88,6 +80,7 @@ function createCard(container, item) {
     title.innerText = item.name;
     price.innerText = "Price: " + item.price + "₪ per kg";
     btn_addItem.innerText = 'Add To Cart';
+    input.innerText = 'Quantity';
     input.placeholder=1;
 
     btn_addItem.onclick = () => {
@@ -104,10 +97,7 @@ function createCard(container, item) {
     container.appendChild(containerDiv);
 }
 
-//Thought of it at night had to try it i added a quantity box which works and takes the right amount entered and if nothing is entered it takes it as a 1
-//You can check it with console
-//You dont have to change your website now :*
-//Love you <3<3<3
+
 function addToShoppingList(item,quantity){
         console.log(item)
    ShoppingList.push({name:item.name,
@@ -139,7 +129,6 @@ function createCartItem(container, item) {
 
 //add rows in cart table Of the choosen Items
 function createShoppingListCart() {
-    // removeAllCartItems();
     ShoppingList=  sessionStorage.getItem('myArray');
     ShoppingList=  JSON.parse(ShoppingList);
     const cart = document.getElementById('shopping-cart');
@@ -156,190 +145,8 @@ function createShoppingListCart() {
         console.log(finalPrice)
         totalContainer.innerText = "Total price: " + finalPrice + "₪";
 
-            // total-price.innerText = totalPrice.price + "₪";
-
 }
 
 function clearShoppingList(){
     sessionStorage.clear();
 }
-//
-// function finishOrderPopUp() {
-//   var popup = document.getElementById("myPopup");
-//   popup.classList.toggle("show");
-//   ShoppingList.Clear();
-// }
-
-// function check(){
-//     const nameInput = document.querySelector('#shopping-cart')
-//     console.log(nameInput)
-//     console.log('hello im here')
-// }
-
-//
-//     const cakesSection = document.getElementById('cakes');
-//     const yeastCakesSection = document.getElementById('yeast-cakes');
-//     const recomendedSection = document.getElementById('recomended');
-//     const cookiesSection = document.getElementById('cookies');
-//     const challahBreadsSection = document.getElementById('challah-breads');
-//
-//     for (let i = 0; i < items.length; i++) {
-//         const item = items[i];
-//         if (item.category === 'yeast') {
-//             createCard(yeastCakesSection, items[i]);
-//         } else if (item.category === 'cookies') {
-//             createCard(cookiesSection, items[i]);
-//         } else if (item.category === 'cakes') {
-//             createCard(cakesSection, items[i])
-//         } else if (item.category === 'bread') {
-//             createCard(challahBreadsSection, items[i])
-//         } else {
-//             createCard(recomendedSection, items[i]);
-//         }
-//     }
-//
-// }
-
-//     this.getBirthYear = function () {
-//         return this.dob.getFullYear()
-//     }
-//
-//     this.getFullName = function () {
-//         return `${this.firstName} ${this.lastName}`
-//     }
-// }
-
-
-
-// // console.log(person1)
-// // console.log(person1.dob.getDay())
-// // console.log(person1.dob.getFullYear())
-// console.log(person1.getBirthYear())
-// console.log(person1.getFullName())
-// console.log(person2)
-/******************************/
-
-// function Person(firstName, lastName, dob) {
-//     this.firstName = firstName
-//     this.lastName = lastName
-//     this.dob = new Date(dob)
-//
-// }
-//
-// const person1 = new Person('Moti', 'Katz', '4-3-1980')
-//
-//
-// Person.prototype.getBirthYear = function () {
-//     return this.dob.getFullYear()
-//     }
-//
-//
-// console.log(person1.getBirthYear())
-// // console.log(person1.getFullName())
-//
-// Person.prototype.getFullName = function () {
-//     return `${this.firstName} ${this.lastName}`
-// }
-//
-// console.log(person1.getFullName())
-
-/******************************/
-
-// class Person {
-//     constructor(firstName, lastName, dob) {
-//         this.firstName = firstName
-//         this.lastName = lastName
-//         this.dob = new Date(dob)
-//     }
-//
-//     getBirthYear() {
-//         return this.dob.getFullYear()
-//     }
-//
-//     getFullName(){
-//         return `${this.firstName} ${this.lastName}`
-//     }
-// }
-//
-// const person1 = new Person('Moti', 'Katz', '4-3-1980')
-//
-// console.log(person1)
-// console.log(person1.getBirthYear())
-// console.log(person1.getFullName())
-
-/******************************/
-
-// console.log(window)
-// console.log('hello console')
-
-// window.alert('this is alert!!!')
-
-// console.log(window.innerHeight)
-// console.log(window.innerWidth)
-
-// window.localStorage['my_key'] = 'my_value'
-// console.log(window.localStorage)
-
-/******************************/
-
-// console.log(document.getElementById('my-form'))
-// const form = document.getElementById('my-form')
-// console.log(form)
-
-// const form = document.querySelector('#my-form')
-// console.log(form)
-// const el_2 = document.querySelector('.items')
-// console.log(el_2)
-// const el_3 = document.querySelector('li')
-// console.log(el_3)
-/******************************/
-// const e1 = document.getElementsByClassName('item')
-// console.log(e1)
-// const e2 = document.getElementsByTagName('li')
-// console.log(e2)
-// const e3 = document.querySelectorAll('.item')
-// console.log(e3)
-// e3[2].innerHTML = 'YUUUUHUUU'
-/******************************/
-
-// const ul = document.querySelector('.items')
-// console.log(ul)
-
-// ul.remove()
-// ul.lastElementChild.remove()
-
-// ul.firstElementChild.textContent = '<h1>This is Item 1</h1>'
-// ul.lastElementChild.innerHTML = '<h1>This is Item 4</h1>'
-// ul[2].innerHTML = '<h2>This is Item 2</h2>'
-
-// const btn = document.querySelector('.btn')
-// btn.style.background = 'red'
-// btn.style.color = 'white'
-
-// btn.addEventListener('click', (e) => {
-//     e.preventDefault()
-//     // console.log('click!')
-//     console.log(e)
-//     console.log(e.target)
-//     console.log(e.target.className)
-//
-//     e.target.classList.add('btn-2')
-//     // e.target.classList.remove('btn')
-//
-//     document.querySelector('#my-form').style.background = '#ccc'
-//     document.querySelector('body').classList.add('bg-dark')
-//     document.querySelector('.items').lastElementChild.innerHTML = '<h1>This is Item 4</h1>'
-//
-// })
-
-// btn.addEventListener('mouseover', (e) => {
-//
-// })
-//
-// btn.addEventListener('mouseout', (e) => {
-//
-// })
-/******************************/
-
-
-/******************************/
